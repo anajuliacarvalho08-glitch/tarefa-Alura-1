f9bcb632bf28527f62efe2cc5a29b33f8557209b
anajuliacarvalho08-glitch <ana.julia.carvalho08@escola.pr.gov.br>
2026-08-11 18:35:39 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function curtir(botao, tipo) {
+    if (botao.style.backgroundColor === "lightgreen") {
+        botao.style.backgroundColor = "";
+    } else {
+        botao.style.backgroundColor = "lightgreen";
+    }
+}
