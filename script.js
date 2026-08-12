a89179fda60dd9fd31794faa942e1273ef0f2d61
anajuliacarvalho08-glitch <ana.julia.carvalho08@escola.pr.gov.br>
2026-08-12 07:59:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-botoes.forEach(function(botao) {
-botao.addEventListener("click", botaoClicado);
 
-function botaoClicado() {
-console.log("fui clicado");
-let texto = botao.querySelector("span");
-texto.textContent++;
-}
-});
